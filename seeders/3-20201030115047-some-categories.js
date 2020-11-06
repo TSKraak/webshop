084eb3dd6d0abdbf4220580d2207179bcc7d5ef6
Sjouke91 <s.bosma1991@gmail.com>
2020-11-06 11:30:15 +0100

    added seeds for project redux

--- seeders/3-20201030115047-some-categories.js
+++ seeders/3-20201030115047-some-categories.js
@@ -6,23 +6,28 @@ module.exports = {
       "categories",
       [
         {
-          name: "Electronics",
-          description: "Something with a cable",
-          imageUrl: "http://electronics",
+          name: "Fitness",
+          description:
+            "strength training, crossfit, personal training, functional fitness, cardio training.",
+          imageUrl:
+            "https://cdn.fitsociety.nl/wp-content/2020/06/back-in-topshape.jpg",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "Furniture",
-          description: "Anything for in your house",
-          imageUrl: "http://furniture",
+          name: "Yoga",
+          description:
+            "Yoga is a systematic practice of physical exercise, breath control, relaxation, diet control, and positive thinking.",
+          imageUrl:
+            "https://www.tinohoogendijk.nl/content/uploads/2019/07/iStock-858115500.jpg",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "Kitchen appliances",
-          description: "Stuff to cook and clean with",
-          imageUrl: "http://kitchen-appliances",
+          name: "Cycling",
+          description:
+            "Cycling, also called bicycling or biking, is the use of bicycles for transport.",
+          imageUrl: "https://img.redbull.com/",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
